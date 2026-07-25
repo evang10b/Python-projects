619fbbf6931bc2375f1a39ee9523735c1e978459
evang10b <akachikanuevangel@gmail.com>
2026-07-25 20:46:54 +0200

    Made it visually appealing adding extra info to each recipe

--- Recipes/recipe-rec.py
+++ Recipes/recipe-rec.py
@@ -26,7 +26,7 @@ def main():
         a_allergies = input("Choose 2nd allergy from list: Nuts, Milk, Eggs, Other: ").lower().strip()
         if t_allergies == "nuts" and a_allergies == "milk" or t_allergies == "milk" and a_allergies == "nuts":
             recommend(*nutmilk_)
-            prefrences("Homemade Baked Apples")
+            prefrences("Avocado Chocolate Pudding")
         elif t_allergies == "nuts" and a_allergies == "eggs" or t_allergies == "eggs" and a_allergies == "eggs":
             recommend(*nutegg_)
             prefrences("Error: There arent any")
@@ -40,36 +40,197 @@ def main():
         print("Why you here then. Get out. It was a trick question")
 
 # List of nut free dessert
-nut_free = [("Cheese sconces,\n"),
-            ("Canberry and coconut cereal bars,\n"),
-            ("Banana Chips,\n"),
-            ("Pitta Pizzas")]
+nut_free = [("""
+    ======================
+    1. Cheese sconces:
+    - Duration: 35 minutes
+    - Key ingredients: Cheddar cheese,
+    Flour, Butter
+    ======================
+"""),
+            ("""
+    ======================
+    2. Canberry and coconut cereal bars:
+    - Duration: 2 hours
+    - Key ingredients: Dried Canberries,
+    Dessicated Coconuts, Crisped Rice 
+    Cereals, Oats
+    ======================
+"""),
+            ("""
+    ======================
+    3. Banana Chips:
+    - Duration: 2 hours approx.
+    - Key ingredients: Banana, Oil
+    ======================
+"""),
+            ("""
+    ======================
+    4. Pitta Pizzas
+    - Duration: 45 minutes
+    - Key ingredients: Pitta Bread, Tomato,
+    Cheese
+    ======================
+""")]
 # List of Milk free dessert
-milk_free = [("- Cinnamon and apply train mix,\n"),
-             ("- Spicy tortilla chips with pomegranate,\n"),
-             ("- Banana Chips,\n"),
-             ("- Spicy Sweet Potato\n")]
+milk_free = [("""
+    ======================
+    Apple Cinnamon Trail Mix:
+    - Duration: 5 minutes
+    - Key ingredients: Apple cinnamon
+    cherios, variety of nuts
+    ======================\n
+"""),
+             ("""
+    ======================
+    Spicy tortilla chips with pomegranate
+    hummus:
+    - Duration: 15 minutes
+    - Key ingredients: Spices, Tortilla,
+    pomegranate seeds
+    ======================\n
+"""),
+             ("""
+    ======================
+    Banana Chips:
+    - Duration: 2 hours
+    - Key ingredients: Banana, Oil
+    ======================\n
+"""),
+             ("""
+    ======================
+    Roasted Spicy Sweet Potato:
+    - Duration: 40 minutes
+    - Key ingredients: Sweet potatoes,
+    Spices
+    ======================
+""")]
 # List of Egg free dessert
-egg_free = [("- Banana Chips,\n"),
-            ("- Spicy Sweet Potato,\n"),
-            ("- Chicken satay wraps,\n"),
-            ("- Teriyaki Salmon,\n")]
+egg_free = [("""
+    ======================
+    Banana Chips:
+    - Duration: 2 hours approx.
+    - Key ingredients:
+    ======================\n
+"""),
+            ("""
+    ======================
+    Spicy Sweet Potato:
+    - Duration: 40 minutes
+    - Key ingredients: Sweet potatos,
+    Spices
+    ======================\n
+"""),
+            ("""
+    ======================
+    Chicken satay:
+    - Duration: 40 minutes
+    - Key ingredients: Chicken,
+    Spices, Honey
+    ======================\n
+"""),
+            ("""
+    ======================
+    Teriyaki Salmon:
+    - Duration: 40 minutes
+    - Key ingredients: Teriyaki,
+    Salmon
+    ======================
+""")]
 
 # Nut and Milk free dessert
-nutmilk_ = [("Banana Chips,\n"),
-            ("Chocolate Water Cake,\n"),
-            ("Homemade Baked Apples,\n"),
-            ("Avocado Chocolate Pudding Fudge,\n")]
+nutmilk_ = [("""
+    ======================
+    Banana Chips:
+    - Duration: 2 hours approx.
+    - Key ingredients: Banana,
+    Oil
+    ======================\n
+"""),
+            ("""
+    ======================
+    Chocolate Water Cake:
+    - Duration: 50 minutes
+    - Key ingredients: Cocoa Powder,
+    Water
+    ======================\n
+"""),
+            ("""
+    ======================
+    Homemade Baked Apples:
+    - Duration: 1 hour
+    - Key ingredients: Apple
+    ======================\n
+"""),
+            ("""
+    ======================
+    Avocado Chocolate Pudding:
+    - Duration: 10 minutes
+    - Key ingredients:
+    =====================
+""")]
 # Nut and egg free dessert
-nutegg_ = [("Rice Pudding,\n"),
-           ("Panna Cotta,\n"),
-           ("Oatmeal Crescent,\n"),
-           ("Depression Cake,\n")]
+nutegg_ = [("""
+    ======================
+    Rice Pudding:
+    - Duration: 2 hours
+    - Key ingredients: Rice pudding,
+    Nutmeg(NOT a nut)
+    ======================\n
+"""),
+           ("""
+    ======================    
+    Panna Cotta:
+    - Duration: 7+ hours
+    - Key ingredients: Milk, Cream,
+    Gelatine Powder, Vanilla
+    ======================\n
+"""),
+           ("""
+    ======================
+    Oatmeal Crescent:
+    - Duration: 2-3 hours
+    - Key inredients: Oats
+    ======================\n
+"""),
+           ("""
+    ======================
+    Depression Cake:
+    - Duration: 40 minutes
+    - Key ingredients: Cocoa Powder
+    ======================
+""")]
 # Milk and egg free dessert
-milkegg_ = [("Berry Crumbles,\n"),
-           ("Banana Chips,\n"),
-           ("Fruit sorbets,\n"),
-           ("Apple Cinnamon Crisps\n")]
+milkegg_ = [("""
+    ======================
+    Berry Crumbles: 
+    - Duration: 45 minutes to 1 hour
+    - Key ingredients: Berries, Sugar,
+      Mangerine, Cornstarch
+    ======================\n
+"""),
+           ("""
+    ======================
+    Banana Chips:
+    - Duration: 2 hours approx.
+    - Key ingredients: Bananas, Oil
+    ======================\n
+"""),
+           ("""
+    ======================
+    Fruit sorbets:
+    - Duration: 6 hours
+    - Key ingredients: Fruit of your 
+      choice, Honey, lemon juice, Water
+    ======================\n
+"""),
+           ("""
+    ======================
+    Apple Cinnamon Crisps:
+    - Duration: 2 hours
+    - Key Ingredients: Apples, Cinnamon
+    ======================
+""")]
 
 # Prefrencing function
 def prefrences(recipe):
